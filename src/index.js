5fd0ff8b3641e3f393d07caa212430867091f3dc
ASHIKEkfrazo <ashik@ekfrazo.in>
2024-04-17 11:23:10 +0530

    Removed Unwanted Code

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import 'antd/dist/reset.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { hydrate, render } from "react-dom";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
